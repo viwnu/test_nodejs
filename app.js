0b49a448a20ed547eb61cabeb5cac52daafcaaab
viwnu <xenmysafitiplace@gmail.com>
2022-05-20 01:34:57 +0300

    change to translit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('ЗДАРОВА! ЗАЕБАЛ!))))');
+  res.end('ZDAROVA! ZAEBAL!))))');
 });
 
 server.listen(port, hostname, () => {
