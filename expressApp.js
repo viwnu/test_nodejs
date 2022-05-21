19195f37c59042d5b12be83d1bd83e2476293997
viwnu <xenmysafitiplace@gmail.com>
2022-05-22 01:53:44 +0300

    express router

--- expressApp.js
+++ expressApp.js
@@ -1,47 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = express.Router()
 
+router.get('/', function (req, res) {
+  res.sendfile('index.html');
+})
 
-app.use(function(req, res){
-    routes = {
-      '/': 'index.html'
-    };
+router.get('/test_nodejs', function (req, res) {
+  res.send('hellow dura4ok');
+})
 
-    let url = req.url;
-
-    if (routes[url] !== undefined) {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.sendfile(route[url]);
-        console.log("Client requested", url);
-    } else {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.send('Привет ДРУГ!');
-        console.log("Client requested", url);
-        // console.log("Client requested page not found - 404 error");
-    }
-});
-
-
-// app.get(url, (req, res) => {
-//
-//   switch(url){
-//         case "/":
-//             res.sendfile('index.html');
-//             res.writeHead(200, {"Content-Type": "text/plain"});
-//             console.log("Client requested /");
-//             break;
-//         case "/node_testjs":
-//             res.send('Привет ДРУГ!profile.html');
-//             res.writeHead(200, {"Content-Type": "text/plain"});
-//             console.log("Client requested /node_testjs");
-//             break;
-//         default:
-//             res.sendfile('404.html');
-//             res.writeHead(404, {"Content-Type": "text/plain"});
-//             console.log("Client requested page not found - 404 error");
-//     };
-// })
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
