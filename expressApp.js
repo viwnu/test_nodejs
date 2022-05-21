c5dfbb0eb916817803982492bbfb03f6b08174aa
viwnu <xenmysafitiplace@gmail.com>
2022-05-21 17:24:26 +0300

    switch cases url

--- expressApp.js
+++ expressApp.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/node_testjs', (req, res) => {
-  res.send('Привет ДРУГ!')
+app.get(url, (req, res) => {
+  switch(url){
+        case "/":
+            res.sendfile('index.html');
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            console.log("Client requested /");
+            break;
+        case "/node_testjs":
+            res.send('Привет ДРУГ!');
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            console.log("Client requested /node_testjs");
+            break;
+        default:
+            res.sendfile('404.html');
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            console.log("Client requested page not found - 404 error");
+    };
 })
 
 app.listen(port, () => {
